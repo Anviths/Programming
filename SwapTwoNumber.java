83cf8ec25c8ab56edbf3a491b7daafaac8686e10
anvith <anwithgowda2@gmail.com>
2026-02-09 12:49:54 +0530

    day 1

---
+++ SwapTwoNumber.java
@@ -0,0 +1,19 @@
+class SwapTwoNumber{
+
+    public static void main(String[] args) {
+        
+        int a=10;
+        int b=20;
+        int c=0;
+
+
+        System.out.println("Before Swapping a ="+a);
+        System.out.println("Before Swapping b ="+b);
+        c=a;
+        a=b;
+        b=c;
+
+        System.out.println("after Swapping a ="+a);
+        System.out.println("after Swapping a ="+b);
+    }
+}
---
+++ SwapTwoNumber.py
@@ -0,0 +1,13 @@
+
+
+a=10
+b=20
+c=0
+print("Before Swapping a =" ,a)
+print("Before Swapping b =",b)
+c=a
+a=b
+b=c
+
+print("after Swapping a =",a)
+print("after Swapping b =",b)
